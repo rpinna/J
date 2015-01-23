0d979258539e049c7db4a26da959aa10c9a8d163
Rob Pinna <rob.pinna@cofamilies.com>
2015-01-22 22:19:01 -0700

    J.Cofamilies package

---
+++ J.Cofamilies/Representations/JCofamily.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cofamilies.J.People.Representations;
+
+namespace Cofamilies.J.Cofamilies.Representations
+{
+  public class JCofamily
+  {
+    // Constructors
+
+    public JCofamily()
+    {
+      people = new List<JPersonInRole>();
+    }
+
+    // Properties
+
+    // ReSharper disable InconsistentNaming
+    public IList<JPersonInRole> people;
+    public string id;
+    public bool isRemoved;
+    public string name;
+    public string ownership;
+    public string primaryCoparentId;
+    public string primaryHouseholdId;
+    public string secondaryCoparentId;
+    public string secondaryHouseholdId;
+  }
+}
