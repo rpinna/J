abeb6b1fc30e43185e4c18d6f536f2d684a49818
Rob Pinna <rob.pinna@cofamilies.com>
2015-01-22 22:21:41 -0700

    J.Perspective

---
+++ J.Perspective/JPerspective.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cofamilies.J.Perspective
+{
+  // Enumerations
+
+  public enum PerspectiveKind
+  {
+    HeadOfHousehold = 1,
+    Child = 2,
+    Caregiver = 3,
+    Professional = 4
+  }
+
+  public enum RootKind
+  {
+    Cofamily = 1,
+    Household = 2
+  }
+
+  public interface IJPerspective
+  {
+    string Id { get; set; }
+    PerspectiveKind Kind { get; set; }
+    RootKind RootKind { get; set; }
+    string RootId { get; set; }
+  }
+
+  public class JPerspective : IJPerspective
+  {
+    public string Id { get; set; }
+    public PerspectiveKind Kind { get; set; }
+    public RootKind RootKind { get; set; }
+    public string RootId { get; set; }
+  }
+}
