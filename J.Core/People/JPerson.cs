3f200fa3c20b271a6e94b94849e347b072003b20
Rob Pinna <rob.pinna@cofamilies.com>
2015-02-18 17:20:10 -0700

    JPerson

---
+++ J.Core/People/JPerson.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Cofamilies.J.Core.People
+{
+  //{"email":"rob.pinna@cofamilies.com",
+  // "firstName":"Rob",
+  // "lastName":"Pinna",
+  // "id":"PERSglmd63prcjdopwuhohw4h7owcgeu",
+  // "avatarUrl":"",
+  // "isDeleted":false,
+  // "isZombie":false,
+  // "memberSince":"2012-02-25T02:51:03.3582813Z",
+  // "timezoneInfoId":"Central Standard Time"}
+
+  public interface IJPerson
+  {
+    string AvatarUrl { get; set; }
+    string Email { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    string Id { get; set; }
+    bool IsDeleted { get; set; }
+    bool IsZombie { get; set; }
+    string MemberSinceISOString { get; set; }
+    string TimeZoneInfoId { get; set; }
+  }
+
+  public class JPerson : IJPerson
+  {
+    [JsonProperty("email")]
+    public string Email { get; set; }
+
+    [JsonProperty("firstName")]
+    public string FirstName { get; set; }
+ 
+    [JsonProperty("lastName")]
+    public string LastName { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("avatarUrl")]
+    public string AvatarUrl { get; set; }
+
+    [JsonProperty("isDeleted")]
+    public bool IsDeleted { get; set; }
+
+    [JsonProperty("isZombie")]
+    public bool IsZombie { get; set; }
+
+    [JsonProperty("memberSince")]
+    public string MemberSinceISOString { get; set; }
+
+    [JsonProperty("timeZoneInfoId")]
+    public string TimeZoneInfoId { get; set; }
+  }
+}
