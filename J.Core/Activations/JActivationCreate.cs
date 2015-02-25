6e04f2f89ecfe5b3a648e6420add4439411351fb
Rob Pinna <rob.pinna@cofamilies.com>
2015-02-24 17:12:48 -0700

    Activations + NuGet spec to remove Visual Studio dependency

---
+++ J.Core/Activations/JActivationCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cofamilies.J.Core.Activations
+{
+  public interface IJActivationCreate
+  {
+  }
+
+  public class JActivationCreate : IJActivationCreate
+  {
+  }
+}
---
+++ J.Core/Activations/JActivationCreateResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cofamilies.J.Core.Activations
+{
+  public interface IJActivationCreateResult
+  {
+  }
+
+  public class JActivationCreateResult : IJActivationCreateResult
+  {
+  }
+}
