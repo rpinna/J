faa16448a7e0b152ffb13a6ae74a9afc8f675adb
Rob Pinna <rob.pinna@cofamilies.com>
2015-03-22 20:37:59 -0600

    Add JDevice

---
+++ J.Core/Devices/JDevice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cofamilies.J.Core.Devices
+{
+  public interface IJDevice
+  {
+    DateTime Created { get; set; }
+    string Id { get; set; }
+    string Name { get; set; }
+    string Os { get; set; }
+    string Status { get; set; }
+    string Token { get; set; }
+  }
+
+  public class JDevice : IJDevice
+  {
+    public DateTime Created { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Os { get; set; }
+    public string PrimaryKey { get { return Id; } }
+    public string Status { get; set; }
+    public string Token { get; set; }
+  }
+}
+
+/*
+ *   public interface IDeviceModel : IClientModel
+  {
+    DateTime Created { get; set; }
+    string Id { get; set; }
+    string Name { get; set; }
+    string Os { get; set; }
+    string Status { get; set; }
+    string Token { get; set; }
+  }
+
+  public class DeviceModel : IDeviceModel
+  {
+    public DateTime Created { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Os { get; set; }
+    public string PrimaryKey { get { return Id; } }
+    public string Status { get; set; }
+    public string Token { get; set; }
+  }*/
---
+++ J.Core/Devices/JDevices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Cofamilies.J.Core.Devices
+{
+  public class JDevices
+  {
+    public JDevices()
+    {
+      Devices = new List<JDevice>();
+    }
+
+    [JsonProperty("devices")]
+    public List<JDevice> Devices { get; private set; } 
+  }
+}
